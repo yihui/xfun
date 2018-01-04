e4af4816cbb26168c2fd6be08ec13367b08614a6
Yihui Xie <xie@yihui.name>
2018-01-04 14:33:34 -0600

    functions isFALSE() and in_dir()

---
+++ R/utils.R
@@ -0,0 +1,26 @@
+#' Test if an object is identical to \code{FALSE}
+#'
+#' A simple abbreviation of \code{identical(x, FALSE)}.
+#' @param An R object.
+#' @export
+#' @examples
+#' library(xfun)
+#' isFALSE(TRUE)  # false
+#' isFALSE(FALSE)  # true
+#' isFALSE(c(FALSE, FALSE))  # false
+isFALSE = function(x) identical(x, FALSE)
+
+#' Evaluate an expression in a specified directory
+#'
+#' Change the working directory, evaluate the expression, and restore the
+#' working directory.
+#' @param dir Path to a directory.
+#' @param expr An R expression.
+#' @export
+#' @examples
+#' library(xfun)
+#' in_dir(tempdir(), {print(getwd()); list.files()})
+in_dir = function(dir, expr) {
+  owd = setwd(dir); on.exit(setwd(owd))
+  expr
+}
