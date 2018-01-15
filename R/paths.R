586d6035212d57ccae25072ea140c38974e084d7
Yihui Xie <xie@yihui.name>
2018-01-15 14:37:26 -0600

    wrapper functions to normalize and compare paths

--- R/paths.R
+++ R/paths.R
@@ -33,3 +33,26 @@ with_ext = function(x, ext) {
   if (n1 > 1 && n1 != n2) stop("'ext' must be of the same length as 'x'")
   mapply(sub, r, ext, x, USE.NAMES = FALSE)
 }
+
+#' Normalize paths
+#'
+#' A wrapper function of \code{normalizePath()} with different defaults.
+#' @param path,winslash,must_work Arguments passed to
+#'   \code{\link{normalizePath}()}.
+#' @export
+#' @examples library(xfun)
+#' normalize_path('~')
+normalize_path = function(path, winslash = '/', must_work = FALSE) {
+  normalizePath(path, winslash = winslash, mustWork = must_work)
+}
+
+#' Test if two paths are the same after they are normalized
+#'
+#' Compare two paths after normalizing them with the same separator (\code{/}).
+#' @param p1,p2 Two vectors of paths.
+#' @export
+#' @examples library(xfun)
+#' same_path('~/foo', file.path(Sys.getenv('HOME'), 'foo'))
+same_path = function(p1, p2) {
+  normalize_path(p1) == normalize_path(p2)
+}
