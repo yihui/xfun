fad98694ab5cbda0f033df38ae634ae16dd22b2d
Yihui Xie <xie@yihui.name>
2018-11-27 09:21:50 -0600

    tweak after #13

--- tests/testit/test-packages.R
+++ tests/testit/test-packages.R
@@ -1,12 +1,12 @@
 library(testit)
 
-assert("loadable works", {
-  (loadable("base") %==% TRUE)
-  (loadable("base", new_session = TRUE) %==% TRUE)
-  (loadable("base", strict = FALSE) %==% TRUE)
+assert("loadable() works", {
+  (loadable("base"))
+  (loadable("base", new_session = TRUE))
+  (loadable("base", strict = FALSE))
   (has_error(loadable(c("base", "base2"))))
   (has_error(loadable(character())))
-  (loadable("#base") %==% FALSE)
-  (loadable("#base", new_session = TRUE) %==% FALSE)
-  (loadable("#base", strict = FALSE) %==% FALSE)
+  (!loadable("#base"))
+  (!loadable("#base", new_session = TRUE))
+  (!loadable("#base", strict = FALSE))
 })
