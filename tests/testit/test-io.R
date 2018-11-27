c7abb8f33260e127aab7323c100d09c4bbf9dd2b
Xianying Tan <shrektan@126.com>
2018-11-28 00:05:47 +0800

    tests for strict_list, raw_string, native_encode and is_ascii  (#7)
    * add the test for strict_list()
    
    * add a test for raw_string()
    
    * add tests for encoding utils
    
    * the test about native_encode() throws warnings should only be run in windows
    
    * the test of is_ascii() should return NA for NA_character_ as well
    
    * change `<-` to `=`
    
    * add tests for read_utf8 and write_utf8

---
+++ tests/testit/test-io.R
@@ -0,0 +1,36 @@
+library(testit)
+
+assert("invalid_utf8 works and respects NA", {
+  (invalid_utf8(character()) %==% integer())
+  x = "fa\xE7ile"; Encoding(x) = "latin1"
+  (invalid_utf8(c("aaa", x, NA_character_, "", "\u4e2d\u6587")) %==% 2L)
+})
+
+assert("read/write_utf8 works", {
+  ascii_txt = c("aa", "bb", "cc")
+  utf8_txt = c("\u4e2d\u6587", "\u5927\u5bb6\u597d", "\u65e9\u996d")
+  latin_txt = local({x = "fa\xE7ile"; Encoding(x) = "latin1"; x})
+  mixed_txt = c(ascii_txt, latin_txt, utf8_txt)
+
+  write_utf8(ascii_txt, con = (ascii_file = tempfile()))
+  write_utf8(utf8_txt, con = (utf8_file = tempfile()))
+  write_utf8(latin_txt, con = (latin_file = tempfile()))
+  write_utf8(mixed_txt, con = (mixed_file = tempfile()))
+
+  (read_utf8(ascii_file) %==% ascii_txt)
+  (read_utf8(utf8_file) %==% utf8_txt)
+  (read_utf8(latin_file) %==% latin_txt) # identical will not compare Encoding
+  (Encoding(read_utf8(latin_file)) %==% "UTF-8")
+  (read_utf8(mixed_file) %==% mixed_txt)
+  (Encoding(read_utf8(mixed_file)) %==% c(rep("unknown", 3), rep("UTF-8", 4)))
+
+  mixed_file2 = tempfile()
+  local({
+    opts = options(encoding = 'native.enc'); on.exit(options(opts), add = TRUE)
+    writeLines(mixed_txt, con = mixed_file2, useBytes = TRUE)
+  })
+  (suppressWarnings(read_utf8(mixed_file2)[4] != mixed_txt[4]))
+  has_warning(read_utf8(mixed_file2))
+  has_error(read_utf8(mixed_file2, error = TRUE))
+})
+
