1479c21467e3f277d09df6b69fcc803dd65e1ccc
Yihui Xie <xie@yihui.name>
2018-11-27 10:21:29 -0600

    cosmetic

--- tests/testit/test-data-structure.R
+++ tests/testit/test-data-structure.R
@@ -1,24 +1,24 @@
 library(testit)
 
-assert("strict_list is really strict", {
-  s_list = strict_list(aaa = 1:3, bbb = c("hey", "mom"))
+assert('strict_list() is really strict', {
+  s_list = strict_list(aaa = 1:3, bbb = c('hey', 'mom'))
   # the class name better not be changed in the future
-  (inherits(s_list, "xfun_strict_list"))
+  (inherits(s_list, 'xfun_strict_list'))
   # `$` returns the expected value if the name provided is complete
   (s_list$aaa %==% 1:3)
   # but partial match is prohibited
   (s_list$a %==% NULL)
   # `[` will return a list - will it be better if returns a strict list?
-  (inherits(s_list["aaa"], "list"))
+  (inherits(s_list['aaa'], 'list'))
   # and add an element won't change the class by accident
   s_list$ccc = 4
-  s_list$ddd = "abcd"
-  (inherits(s_list, "xfun_strict_list"))
+  s_list$ddd = 'abcd'
+  (inherits(s_list, 'xfun_strict_list'))
 })
 
-assert("raw_string prints as expected", {
+assert('raw_string() prints as expected', {
   rs = raw_string(c('a "b"', 'hello\tworld!'))
-  (inherits(rs, "xfun_raw_string"))
+  (inherits(rs, 'xfun_raw_string'))
   output = capture.output(rs)
-  (output %==% c("a \"b\"", "hello\tworld!"))
+  (output %==% c('a "b"', 'hello\tworld!'))
 })
--- tests/testit/test-paths.R
+++ tests/testit/test-paths.R
@@ -1,20 +1,20 @@
 library(testit)
 
-assert("with_ext works for corner cases", {
-  (with_ext(character(), "abc") %==% character())
-  (with_ext("abc", character()) %==% "abc")
-  (with_ext(NA_character_, "abc") %==% NA_character_)
-  (has_error(with_ext("abc", NA_character_)))
-  (with_ext("abc", c("d", "e")) %==% c("abc.d", "abc.e"))
-  (has_error(with_ext(c("a", "b"), c("d", "e", "f"))))
-  (with_ext(c("a", "b"), c("d", "e")) %==% c("a.d", "b.e"))
-  (with_ext(c("a", "b"), c("d")) %==% c("a.d", "b.d"))
-  (with_ext(c("a", "b", "c"), c("", ".d", "e.e")) %==% c("a", "b.d", "c.e.e"))
+assert('with_ext works for corner cases', {
+  (with_ext(character(), 'abc') %==% character())
+  (with_ext('abc', character()) %==% 'abc')
+  (with_ext(NA_character_, 'abc') %==% NA_character_)
+  (has_error(with_ext('abc', NA_character_)))
+  (with_ext('abc', c('d', 'e')) %==% c('abc.d', 'abc.e'))
+  (has_error(with_ext(c('a', 'b'), c('d', 'e', 'f'))))
+  (with_ext(c('a', 'b'), c('d', 'e')) %==% c('a.d', 'b.e'))
+  (with_ext(c('a', 'b'), c('d')) %==% c('a.d', 'b.d'))
+  (with_ext(c('a', 'b', 'c'), c('', '.d', 'e.e')) %==% c('a', 'b.d', 'c.e.e'))
 })
 
-assert("same_path works", {
-  (same_path("~/foo", NA_character_) %==% NA)
-  (same_path(NA_character_, "~/foo") %==% NA)
+assert('same_path works', {
+  (is.na(same_path('~/foo', NA_character_)))
+  (is.na(same_path(NA_character_, '~/foo')))
   (same_path('~/foo', file.path(Sys.getenv('HOME'), 'foo')))
-  !same_path(tempdir(), "foo")
+  (!same_path(tempdir(), 'foo'))
 })
