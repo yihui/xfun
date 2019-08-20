c80951b21623680952f34b4927861b5177725cb7
Kishore Vancheeshwaran <24776049+kishvanchee@users.noreply.github.com>
2019-08-20 21:03:49 +0530

    added tests for protect_math()

--- tests/testit/test-markdown.R
+++ tests/testit/test-markdown.R
@@ -44,3 +44,26 @@ assert('prose_index() works', {
   (has_warning(prose_index(x)))
   (prose_index(x) %==% out)
 })
+
+
+assert('protect_math() puts inline math expressions in backticks', {
+  (protect_math('$x$') %==% '`\\(x\\)`')
+
+  (protect_math('$ x$') %==% '$ x$')
+
+  (protect_math('$x $') %==% '$x $')
+
+  (protect_math('hi $$\alpha$$') %==% 'hi `$$\alpha$$`')
+
+  (protect_math('hi $$\alpha $$') %==% 'hi $$\alpha $$')
+
+  (protect_math('hi $$ \alpha$$') %==% 'hi $$ \alpha$$')
+
+  (protect_math('hi $x$9') %==% 'hi $x$9')
+
+  (protect_math('hi $x$a') %==% 'hi `\\(x\\)`a')
+
+  (protect_math('  $x$ starts with 2 spaces') %==% ' $x$ starts with 2 spaces')
+
+
+})
