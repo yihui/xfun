30a1a7b1aae371fb7d41b5f22701c2c76be4d3e6
Yihui Xie <xie@yihui.name>
2020-06-20 15:20:07 -0500

    unnecessary all()

--- tests/testit/test-base64.R
+++ tests/testit/test-base64.R
@@ -5,11 +5,11 @@ assert('base64_encode() encodes the string correctly', {
     "AQ==", "AQI=", "AQID", "AQIDBA==", "AQIDBAU=", "AQIDBAUG",
     "AQIDBAUGBw==", "AQIDBAUGBwg=", "AQIDBAUGBwgJ", "AQIDBAUGBwgJCg=="
   )
-  (all(sapply(1:10, function(i) base64_encode(as.raw(1:i))) == ref))
+  (sapply(1:10, function(i) base64_encode(as.raw(1:i))) == ref)
 
   ref = c(
     "/w==", "//4=", "//79", "//79/A==", "//79/Ps=", "//79/Pv6",
     "//79/Pv6+Q==", "//79/Pv6+fg=", "//79/Pv6+fj3", "//79/Pv6+fj39g=="
   )
-  (all(sapply(255:246, function(i) base64_encode(as.raw(255:i))) == ref))
+  (sapply(255:246, function(i) base64_encode(as.raw(255:i))) == ref)
 })
