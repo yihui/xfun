a8c7dce6ea4defdad89a456349640ff126ffd839
Yihui Xie <xie@yihui.name>
2020-06-20 16:46:02 -0500

    add a new function base64_uri() to generate the data URI for a file (https://en.wikipedia.org/wiki/Data_URI_scheme)

--- R/base64.R
+++ R/base64.R
@@ -13,3 +13,19 @@ base64_encode = function(x) {
   if (!is.raw(x)) x = read_bin(x)
   .Call('base64_enc', x)
 }
+
+#' Generate the Data URI for a file
+#'
+#' Encode the file in the base64 encoding, and add the media type. The data URI
+#' can be used to embed data in HTML documents, e.g., in the \code{src}
+#' attribute of the \verb{<img />} tag.
+#' @param x A file path.
+#' @return A string of the form \verb{data:<media type>;base64,<data>}.
+#' @export
+#' @examples
+#' logo = file.path(R.home('doc'), 'html', 'logo.jpg')
+#' img = htmltools::img(src = xfun::base64_uri(logo), alt = 'R logo')
+#' if (interactive()) htmltools::browsable(img)
+base64_uri = function(x) {
+  paste0("data:", mime::guess_type(x), ";base64,", base64_encode(x))
+}
