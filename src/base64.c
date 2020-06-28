9a1149f65bdf6092a15c75a979a215d856128557
Wush Wu <wush978@gmail.com>
2020-06-28 20:52:29 +0800

    implement base64 decoding function

--- src/base64.c
+++ src/base64.c
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include <string.h>
 #include <R.h>
 #include <Rdefines.h>
 #include <Rversion.h>
@@ -16,6 +17,7 @@ const char base64_table[65] = {
 };
 
 const char padding = '=';
+const unsigned char upadding = '=';
 
 void base64_encode_impl(
   const unsigned char* input,
@@ -49,6 +51,84 @@ void base64_encode_impl(
   }
 }
 
+static const short base64_reverse_table[256] = { -2, -2, -2, -2, -2, -2, -2, -2,
+		-2, -1, -1, -2, -2, -1, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2,
+		-2, -2, -2, -2, -2, -2, -1, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, 62,
+		-2, -2, -2, 63, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, -2, -2, -2, -2,
+		-2, -2, -2, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16,
+		17, 18, 19, 20, 21, 22, 23, 24, 25, -2, -2, -2, -2, -2, -2, 26, 27, 28,
+		29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46,
+		47, 48, 49, 50, 51, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2,
+		-2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2,
+		-2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2,
+		-2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2,
+		-2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2,
+		-2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2,
+		-2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2,
+		-2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2 };
+
+int base64_decode_impl(
+  const unsigned char* input,
+  R_xlen_t input_len,
+  unsigned char* output,
+  const R_xlen_t output_len
+) {
+  for (R_xlen_t i = 0;i < output_len;i++) {
+    output[i] = 0;
+  }
+  int ch = 0;
+  R_xlen_t i = 0, j = 0, k = 0;
+  while(input_len-- > 0) {
+    ch = *input++;
+    if (ch == padding) {
+      if (*input != upadding && (i % 4) == 1) {
+        return 1;
+      }
+      continue;
+    }
+    ch = base64_reverse_table[ch];
+    if (ch == -1) {
+      continue;
+    }
+    else if (ch == -2) {
+      return 1;
+    }
+    switch (i % 4) {
+      case 0 : {
+        output[j] = ch << 2;
+        break;
+      }
+      case 1 : {
+        output[j++] |= ch >> 4;
+        output[j] = (ch & 0x0f) << 4;
+        break;
+      }
+      case 2 : {
+        output[j++] |= ch >> 2;
+        output[j] = (ch & 0x03) << 6;
+        break;
+      }
+      case 3 : {
+        output[j++] |= ch;
+        break;
+      }
+    }
+    i++;
+  }
+  k = j;
+  if (ch == padding ) {
+    switch (i % 4) {
+      case 1 : {
+        return 1;
+      }
+      case 2 : k++;
+      case 3 : output[k] = 0;
+    }
+  }
+  if (j != output_len) return 1;
+  return 0;
+}
+
 SEXP base64_enc(SEXP input) {
   int rv = 0;
   // get input
@@ -65,7 +145,8 @@ SEXP base64_enc(SEXP input) {
   unsigned char *input_content = (unsigned char*) RAW(input);
   // declare output
   // allocate memory
-  SEXP result = PROTECT(NEW_CHARACTER(1));
+  SEXP result = R_NilValue;
+  result = PROTECT(NEW_CHARACTER(1));
   if (result) { // check if memory is allocated successfully
     char* result_content = (char*) malloc(output_len + 1);
     if (result_content) { // check if memory is allocated successfully
@@ -78,6 +159,7 @@ SEXP base64_enc(SEXP input) {
     }
   } else {
     rv = 1;
+    result = R_NilValue;
   }
   UNPROTECT(1);
   switch (rv) {
@@ -94,3 +176,63 @@ SEXP base64_enc(SEXP input) {
   return result;
 }
 
+SEXP base64_dec(SEXP input) {
+  int rv = 0;
+  R_xlen_t input_len;
+  SEXP result = R_NilValue;
+#if defined(R_VERSION) && R_VERSION >= R_Version(3,0,0)
+  input_len = XLENGTH(input);
+#else
+  input_len = LENGTH(input);
+#endif
+  if (input_len != 1 || TYPEOF(input) != STRSXP) {
+    rv = 1;
+  } else {
+    SEXP input_char = STRING_ELT(input, 0);
+    const unsigned char* input_p = (unsigned char*) CHAR(input_char);
+    const R_xlen_t input_str_size = strlen((const char*) input_p);
+    if (input_str_size % 4 != 0) {
+      rv = 2;
+    } else {
+      R_xlen_t output_len = input_str_size / 4 * 3;
+      if (input_str_size > 0 && input_p[input_str_size - 1] == upadding) {
+        output_len -= 1;
+        if (input_p[input_str_size - 2] == upadding) output_len -= 1;
+      }
+      result = PROTECT(NEW_RAW(output_len));
+      if (result) {
+        unsigned char* result_content = RAW_POINTER(result);
+        if (base64_decode_impl(
+          input_p,
+          input_str_size,
+          result_content,
+          output_len
+        ) != 0) {
+          rv = 2;
+          UNPROTECT(1);
+          result = R_NilValue;
+        } else {
+          UNPROTECT(1);
+        }
+      } else {
+        rv = 3;
+        result = R_NilValue;
+      }
+    }
+  }
+  switch (rv) {
+    case 1 : {
+      error("The input should be a character vector with length 1");
+      break;
+    }
+    case 2 : {
+      error("The input string is not a valid base64 encoded string");
+      break;
+    }
+    case 3 : {
+      error("Failed to allocate memory for result");
+      break;
+    }
+  }
+  return result;
+}
--- tests/testit/test-base64.R
+++ tests/testit/test-base64.R
@@ -14,6 +14,15 @@ assert('base64_encode() encodes the string correctly', {
   (sapply(255:246, function(i) base64_encode(as.raw(255:i))) == ref)
 })
 
+assert('base64_decode() decoes the string correctly', {
+  sapply(c(1:10, 255:246), function(i) {
+    input <- as.raw(1:i)
+    output <- base64_encode(input)
+    input2 <- base64_decode(output)
+    isTRUE(all.equal(input, input2))
+  })
+})
+
 assert('base64_encode_r() returns the same result as base64_encode()', {
   f = R_logo()
   base64_encode_r(f) %==% base64_encode(f)
