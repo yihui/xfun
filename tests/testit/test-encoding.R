6dfecca68f5d9c9b76d2ba2301f23fe39441e6d7
Yihui Xie <xie@yihui.name>
2021-08-05 17:37:58 -0500

    fix a test on r-devel-windows-x86_64-gcc10-UCRT

--- tests/testit/test-encoding.R
+++ tests/testit/test-encoding.R
@@ -1,10 +1,11 @@
 library(testit)
 
 assert('native_encode() warns against characters that cannot be represented in native encoding', {
-  latin_str = 'fa\u00e7ile'
-  cn_str = '\u4e2d\u6587\u5b57\u7b26'
-  # I bet only one or none of latin1 & GBK will be the native encoding in windows
-  (!is_windows() || has_warning({native_encode(cn_str); native_encode(latin_str)}))
+  latin_str = 'fa\u00e7ile'  # Latin-1
+  cn_str = '\u4e2d\u6587\u5b57\u7b26'  # UTF-8 Chinese
+  # when the locale is not UTF-8, the above strings cannot be converted to native encoding
+  (isTRUE(l10n_info()[['UTF-8']]) ||
+      has_warning({native_encode(cn_str); native_encode(latin_str)}))
 
   gb2312_raw = as.raw(c(0xd6, 0xd0, 0xce, 0xc4, 0xd7, 0xd6, 0xb7, 0xfb))
   is_gb2312_native = identical(gb2312_raw, charToRaw(enc2native(cn_str)))
