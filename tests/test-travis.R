2e7653ecd51a24f8af906f110bc4b8a4347ab5ac
etiennebacher <you@example.com>
2021-09-02 13:52:18 +0200

    move repo options in test-travis.R

--- tests/test-travis.R
+++ tests/test-travis.R
@@ -1,2 +1,3 @@
 # tests needing an internet connection
+options(repos = c(CRAN = "https://cran.rstudio.com"))
 if (!is.na(Sys.getenv('CI', NA))) testit::test_pkg('xfun', 'test-travis')
--- tests/test-travis/test-revcheck.R
+++ tests/test-travis/test-revcheck.R
@@ -1,7 +1,6 @@
 library(testit)
 
 assert("check_deps() works", {
-  options(repos = c(CRAN = "https://cran.rstudio.com"))
   x <- check_deps("xfun")
   (is.character(x$check))
   (is.character(x$install))
