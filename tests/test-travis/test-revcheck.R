0eae8628fdb21f17da60f94a84edfbb9b8c460e7
etiennebacher <you@example.com>
2021-09-02 11:53:46 +0200

    set cran repo option

--- tests/test-travis/test-revcheck.R
+++ tests/test-travis/test-revcheck.R
@@ -1,6 +1,7 @@
 library(testit)
 
 assert("check_deps() works", {
+  options(repos = c(CRAN = "https://cran.rstudio.com"))
   x <- check_deps("xfun")
   (is.character(x$check))
   (is.character(x$install))
