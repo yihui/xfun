75c39a37d695b163a32b8e21edf68a99b450b2fe
Yihui Xie <xie@yihui.name>
2021-09-15 16:47:51 -0500

    don't hard-code maintainer (read it from package description instead)

--- tests/test-cran/test-cran.R
+++ tests/test-cran/test-cran.R
@@ -1,9 +1,9 @@
 library(testit)
 
 assert("pkg_maintainers() works", {
-  x <- pkg_maintainers("xfun")
+  x = pkg_maintainers("xfun")
   (length(x) > 0)
   # %==% not working for some reason
-  (x[1] == "Yihui Xie <xie@yihui.name>")
+  (x %==% c(xfun = packageDescription('xfun', fields = 'Maintainer')))
 })
 
