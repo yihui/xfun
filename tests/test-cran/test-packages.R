ab79b83e13aa793cc9d36b8e195b4e7310799c9a
Yihui Xie <xie@yihui.name>
2021-09-15 21:17:44 -0500

    remove the test of pkg_needs_compilation()

--- tests/test-cran/test-packages.R
+++ tests/test-cran/test-packages.R
@@ -15,7 +15,3 @@ assert("major_mninor_smaller() works", {
   (major_minor_smaller(as.numeric_version("4.1.0"), as.numeric_version("4.2.0")))
   (!major_minor_smaller(as.numeric_version("4.1.0"), as.numeric_version("4.1.1")))
 })
-
-assert("pkg_needs_compilation() returns a character vector", {
-  (is.character(pkg_needs_compilation()))
-})
