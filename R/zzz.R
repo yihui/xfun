8d7e092553d845ccebbaa8bd21847e41fa554385
Yihui Xie <xie@yihui.name>
2024-08-02 01:39:47 -0500

    remove functions that are available in R 3.2.0

--- R/zzz.R
+++ R/zzz.R
@@ -8,14 +8,6 @@ if (!has_fun('endsWith')) endsWith = function(x, s) {
   n = nchar(x)
   substring(x, n - nchar(s) + 1, n) == s
 }
-if (!has_fun('anyNA')) anyNA = function(x) any(is.na(x))
-if (!exists('trimws', baseenv(), inherits = FALSE)) trimws = function(x, which = 'both') {
-  r = c(both = '^\\s+|\\s+$', right = '\\s+$', left = '^\\s+')[which]
-  gsub(r, '', x)
-}
-if (!exists('file.size', baseenv(), inherits = FALSE)) file.size = function(...) {
-  file.info(...)$size
-}
 if (!has_fun('strrep')) strrep = function(x, times) {
   mapply(function(x, n) paste(rep(x, n), collapse = ''), x, times, USE.NAMES = FALSE)
 }
