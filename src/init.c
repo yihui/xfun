def9f9196ab3e2b8b38174384e1730390cba7ae1
Yihui Xie <xie@yihui.name>
2025-03-17 23:41:59 -0500

    a simple implementation of LCG
    this is primarily for me to practice C, not intended for serious purposes

--- src/init.c
+++ src/init.c
@@ -6,10 +6,12 @@
 /* .Call calls */
 extern SEXP base64_enc(SEXP);
 extern SEXP base64_dec(SEXP);
+extern SEXP rand_lcg(SEXP, SEXP, SEXP, SEXP, SEXP);
 
 static const R_CallMethodDef CallEntries[] = {
   {"base64_enc", (DL_FUNC) &base64_enc, 1},
   {"base64_dec", (DL_FUNC) &base64_dec, 1},
+  {"rand_lcg",   (DL_FUNC) &rand_lcg,   5},
   {NULL, NULL, 0}
 };
 
