87c7af9f0a9e5c5c3f5f5f37e94a8cd7ac7709f3
Yihui Xie <xie@yihui.name>
2025-08-18 13:20:17 -0500

    add a test for record() with raw strings

--- tests/test-cran/test-record.R
+++ tests/test-cran/test-record.R
@@ -6,3 +6,9 @@ assert('record() works', {
   (record2('quote(x)') %==% c('quote(x)', 'x'))
   (record2('quote(x+y)') %==% c('quote(x+y)', 'x + y'))
 })
+
+assert('record() handles raw_string objects', {
+  s = raw_string(c('line 1', 'line 2\nwith newline', 'line 3 "with quotes"'))
+  rec = record('s')
+  (as.character(rec[[2]]) %==% as.character(s))
+})
