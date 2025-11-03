b60f1eec43fbe0fa0a1e561d51b433df255a0086
Yihui Xie <xie@yihui.name>
2025-11-02 23:15:47 -0600

    add isFALSE()

--- R/zzz.R
+++ R/zzz.R
@@ -1,6 +1,9 @@
 has_fun = function(name, envir = baseenv()) exists(name, envir, inherits = FALSE)
 
 # for R < 4.0
+if (!has_fun('isFALSE')) isFALSE = function(x) {
+  is.logical(x) && length(x) == 1 && !is.na(x) && !x
+}
 if (!has_fun('startsWith')) startsWith = function(x, s) {
   substring(x, 1, nchar(s)) == s
 }
