a55a914523547af1c10a7c9d057e70f17c3f52be
Yihui Xie <xie@yihui.name>
2025-11-02 23:22:52 -0600

    conditional test

--- tests/test-ci/test-cran.R
+++ tests/test-ci/test-cran.R
@@ -1,6 +1,6 @@
 library(testit)
 
-assert("pkg_maintainers() works", {
+if (exists('CRAN_package_db', asNamespace('tools'))) assert("pkg_maintainers() works", {
   x = pkg_maintainers("xfun")
   (length(x) > 0)
   # %==% not working for some reason
