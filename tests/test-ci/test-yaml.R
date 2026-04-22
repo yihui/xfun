eb0e7ea11146935360345edb799cadfe394f2659
Yihui Xie <xie@yihui.name>
2026-04-21 19:26:45 -0500

    nonsense

--- tests/test-ci/test-yaml.R
+++ tests/test-ci/test-yaml.R
@@ -15,24 +15,3 @@ if (loadable('yaml')) assert('yaml_load() shows helpful error message on invalid
   )
   (gsub('^Failed to parse YAML:.*', '', msgs) %==% '')
 })
-
-# test yaml_load() error handler when location info is present
-if (loadable('yaml')) assert('yaml_load() error handler returns NULL for non-parseable error msgs', {
-  # when the error message has no line/column info, handler returns NULL
-  msgs = c()
-  tryCatch(
-    withCallingHandlers(
-      # force a yaml error that won't have standard line/column info by
-      # using an invalid handler that raises an error without line info
-      handle_error(
-        stop('some yaml error without line info'),
-        function(loc) NULL
-      ),
-      message = function(m) {
-        msgs <<- c(msgs, conditionMessage(m)); invokeRestart('muffleMessage')
-      }
-    ),
-    error = function(e) NULL
-  )
-  TRUE  # just verify no crash
-})
